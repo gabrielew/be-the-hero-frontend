f4c1115aa91c65db894e025f560eb47534cea7a7
gabrielew <gabrielalcantaratp@gmail.com>
2020-03-25 10:23:47 -0300

    adding more global styles

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Logon} />
+        <Route path="/" component={Logon} exact />
         <Route path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
