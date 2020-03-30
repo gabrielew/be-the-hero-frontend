066b6c8a75e9c46ed1824f43885ec53bc291a49e
gabrielew <gabrielalcantaratp@gmail.com>
2020-03-30 08:42:20 -0300

    adding toastify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./global.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Routes from "./routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ToastContainer autoClose={3000} />
+    </>
+  );
 }
 
 export default App;
